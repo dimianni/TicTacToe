6a3cd7339fcbc2bbf0fd1ca8b9591639142488a7
Dmytro Anikin <dimianni@users.noreply.github.com>
2021-04-24 16:36:12 +0200

    Upd

--- app/js/main.js
+++ app/js/main.js
@@ -1,76 +1,104 @@
+
 document.addEventListener("DOMContentLoaded", function () {
 
-    const figures = ["images/cross.svg", "images/circle.svg"],
-        classes = ["cross", "circle"],
-        cells = document.querySelectorAll(".board-row__el"),
-        player = document.querySelector(".turn-who"),
-        winner = document.querySelector(".winner");
+    const cells = document.querySelectorAll(".board-cell");
+    const turn = document.querySelector(".turn");
+    const reset = document.querySelector(".reset");
 
-    // Default
-    let turn = 1;
-    player.innerHTML = turn;
 
+    function Player(id, img, className) {
+        this.id = id
+        this.img = img
+        this.className = className
+    }
+
+    const player1 = new Player(1, "images/cross.svg", "cross")
+    const player2 = new Player(2, "images/circle.svg", "circle")
+
+
+    // On cell click
     cells.forEach(cell => {
-
         cell.addEventListener("click", function () {
-
-            const self = this,
-                img = this.querySelector("img")
-
-                
-            const check = el => self.classList.contains(el);
-            if (classes.some(check)) {
-                console.log("already taken");
-                return;
-            }
-
-            // if it is player 1 turn...
-            if (turn % 2 !== 0) {
-
-                // put a cross on cell click
-                img.src = figures[0];
-                this.classList.add("cross")
-                player.innerHTML = 2;
-
-            } else {
-
-                img.src = figures[1];
-                this.classList.add("circle");
-                player.innerHTML = 1;
-
-            }
-
-            checkWinner()
-
-            turn++;
-
+            move(this);
+            checkWinner();
         })
-
     })
 
+    // On reset click
+    reset.addEventListener("click", function() {
+       location.reload()
+    })
+
+    // Default
+    let current = 1;
+    turn.innerHTML = `Player <span class="active-turn">${current}</span> turn`
+
+    function move(cell) {
+
+        if (current === 1) {
+            cell.querySelector("img").src = player1.img
+            cell.classList.add(player1.className)
+        } else {
+            cell.querySelector("img").src = player2.img
+            cell.classList.add(player2.className)
+        }
+
+        current === 1 ? current = 2 : current = 1;
+        turn.innerHTML = `Player <span class="active-turn">${current}</span> turn`
+    }
+
+    function checkWinner() {
+
+        // Loop through arrays of possible combinations
+        for (let i = 0; i < combinations.length; i++) {
+
+            // Winning combination cells
+            let cell1 = cells[combinations[i][0]];
+            let cell2 = cells[combinations[i][1]];
+            let cell3 = cells[combinations[i][2]];
+
+            if (cell1.classList.contains("cross") && cell2.classList.contains("cross") && cell3.classList.contains("cross")) {
+                turn.innerHTML = `<div class="winner">Player ${player1.id} won!</div>`
+
+                highlight([cell1, cell2, cell3])
+                reset.classList.add("active")
+            }
+
+            if (cell1.classList.contains("circle") && cell2.classList.contains("circle") && cell3.classList.contains("circle")) {
+                turn.innerHTML = `<div class="winner">Player ${player2.id} won!</div>`
+
+                highlight([cell1, cell2, cell3])
+                reset.classList.add("active")
+            }
+        }
+
+        // Check for a draw
+        const cellsArr = [];
+        for (let i = 0; i < cells.length; i++) {
+            cellsArr.push(cells[i])
+        }
+        if (cellsArr.every(el => el.classList.contains(player1.className) || el.classList.contains(player2.className))) {
+            turn.innerHTML = "Draw!"
+            reset.classList.add("active")
+        }
+
+    }
 
     const combinations = [
-        [0, 1, 2],
+        [0, 1, 2], // <- for each of these arrays check if corresponding ids have same class (cross/circle)
         [3, 4, 5],
         [6, 7, 8],
         [0, 3, 6],
         [1, 4, 7],
         [2, 5, 8],
         [0, 4, 8],
-        [2, 4, 6],
+        [2, 4, 6]
     ]
 
-    const checkWinner = () => {
-
-        for (let i = 0; i < combinations.length; i++) {
-            if (cells[combinations[i][0]].classList.contains("cross") && cells[combinations[i][1]].classList.contains("cross") && cells[combinations[i][2]].classList.contains("cross")) {
-                winner.classList.add("active")
-            }
-            if (cells[combinations[i][0]].classList.contains("circle") && cells[combinations[i][1]].classList.contains("circle") && cells[combinations[i][2]].classList.contains("circle")) {
-                winner.classList.add("active")
-            }
-        }
-
+    function highlight(arr) {
+        arr.forEach(el => {
+            el.classList.add("winning-cell")
+        })
     }
 
 })
